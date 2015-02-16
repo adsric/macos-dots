8bec1bcd3c945884faa497f268fe341d7dcd057d
Adam Richardson <adsric@icloud.com>
2015-02-16 11:39:49 +0000

    more specific `dotfiles` setup

---
+++ os_x/create_directories.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")" && source "utils.sh"
+
+declare -a DIRECTORIES=(
+    "$HOME/Archive"
+    "$HOME/Projects"
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+    for i in ${DIRECTORIES[@]}; do
+        mkd "$i"
+    done
+}
+
+main
---
+++ os_x/install_npm_packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")" && source "utils.sh"
+
+declare -r -a NPM_PACKAGES=(
+    "gulp"
+    "pageres"
+    "vtop"
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    # Make sure the most recent version of `npm` is installed
+    execute "npm install --global npm" "npm (update)"
+
+    # Install the `npm` packages
+    for i in ${NPM_PACKAGES[@]}; do
+        execute "npm install --global $i" "$i"
+    done
+
+}
+
+main
