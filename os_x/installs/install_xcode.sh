75e72c165a634f3c961b9ae45ee90f10c271b5d1
adsric <adsric@gmail.com>
2015-10-21 11:59:35 +0100

    [osx] Make minor improvements in `install_xcode.sh`

--- os_x/installs/install_xcode.sh
+++ os_x/installs/install_xcode.sh
@@ -7,15 +7,17 @@ cd "$(dirname "$BASH_SOURCE")" \
 
 main() {
 
-    if ! xcode-select -p &> /dev/null; then
+    if ! xcode-select --print-path &> /dev/null; then
 
         # Prompt user to install the XCode Command Line Tools
+
         xcode-select --install &> /dev/null
 
         # ----------------------------------------------------------------------
 
         # Wait until the XCode Command Line Tools are installed
-        while ! xcode-select -p &> /dev/null; do
+
+        until xcode-select --print-path &> /dev/null; do
             sleep 5
         done
 
