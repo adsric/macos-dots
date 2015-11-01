4985c321d6ec7d8cd95f44265e31e3b1a253de9f
adsric <adsric@gmail.com>
2015-11-01 14:08:32 +0000

    [osx] Stop iTunes from auto syncing devices

--- os_x/set_preferences.sh
+++ os_x/set_preferences.sh
@@ -15,6 +15,7 @@ main() {
     ./preferences/set_dashboard_preferences.sh
     ./preferences/set_dock_preferences.sh
     ./preferences/set_finder_preferences.sh
+    ./preferences/set_itunes_preferences.sh
     ./preferences/set_keyboard_preferences.sh
     ./preferences/set_mail_preferences.sh
     ./preferences/set_messages_preferences.sh
