0180a63d8177ab8c8f702583ddfaf0626f2944ab
Adam Richardson <adam.rickardson@gmail.com>
2016-02-22 10:28:03 +0000

    [osx] Simplify install structure

--- os_x/install_applications.sh
+++ os_x/install_applications.sh
@@ -9,14 +9,14 @@ main() {
 
     # XCode Command Line Tools
 
-    ./install_xcode.sh
+    ./installs/install_xcode.sh
     print_in_green '\n  ---\n\n'
 
     # --------------------------------------------------------------------------
 
     # Homebrew
 
-    ./install_homebrew.sh
+    ./installs/install_homebrew.sh
     print_in_green '\n  ---\n\n'
 
     # --------------------------------------------------------------------------
@@ -33,34 +33,7 @@ main() {
 
     # Homebrew Packages
 
-    brew_install 'Dark Mode' 'dark-mode'
-    brew_install 'FFmpeg' 'ffmpeg --with-fdk-aac --with-libvpx --with-x265'
-    brew_install 'Git' 'git'
-    brew_install 'ImageMagick' 'imagemagick --with-webp'
-    brew_install 'node' 'node'
-    brew_install 'ssh copy id' 'ssh-copy-id'
-    brew_install 'tmux' 'tmux'
-    brew_install 'tree' 'tree'
-    brew_install 'TTF/OTF → WOFF (Zopfli)' 'sfnt2woff-zopfli' 'bramstein/webfonttools'
-    brew_install 'TTF/OTF → WOFF' 'sfnt2woff' 'bramstein/webfonttools'
-    brew_install 'WOFF2' 'woff2' 'bramstein/webfonttools'
-    brew_install 'Vim' 'vim --override-system-vi'
-    brew_install 'Zopfli' 'zopfli'
-
-    brew_install 'Atom' 'atom' 'caskroom/cask' 'cask'
-    brew_install 'Chrome' 'google-chrome' 'caskroom/cask' 'cask'
-    brew_install 'Chrome Canary' 'google-chrome-canary' 'caskroom/cask' 'cask'
-    brew_install 'Firefox' 'firefox' 'caskroom/cask' 'cask'
-    brew_install 'Firefox Developer' 'firefoxdeveloperedition' 'caskroom/versions' 'cask'
-    brew_install 'Firefox Nightly' 'firefox-nightly' 'caskroom/cask' 'cask'
-    # brew_install 'Opera' 'opera' 'caskroom/cask' 'cask'
-    # brew_install 'Opera Beta' 'opera-beta' 'caskroom/cask' 'cask'
-    # brew_install 'Opera Developer' 'opera-developer' 'caskroom/cask' 'cask'
-    brew_install 'Sequel Pro' 'sequel-pro' 'caskroom/cask' 'cask'
-    brew_install 'Unarchiver' 'the-unarchiver' 'caskroom/cask' 'cask'
-    brew_install 'VirtualBox' 'virtualbox' 'caskroom/cask' 'cask'
-    # brew_install 'WebKit' 'webkit-nightly' 'caskroom/cask' 'cask'
-
+    ./installs/install_brew_packages.sh
     print_in_green '\n  ---\n\n'
 
     # --------------------------------------------------------------------------
@@ -97,7 +70,7 @@ main() {
 
     # NPM Packages
 
-    ./install_npm_packages.sh
+    ./installs/install_npm_packages.sh
 
 }
 
