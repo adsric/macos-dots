d4ae0efd2be90378480d72d85631826ab08d1c97
Adam Richardson <adam.rickardson@gmail.com>
2016-05-04 21:02:13 +0100

    [osx] Opt-out of Homebrew's analytics  " ...to opt-out of Homebrew's analytics you may set
            `HOMEBREW_NO_ANALYTICS=1`
    
       in your environment or run
    
            `git config --file="$(brew --repository)/.git/config" --replace-all homebrew.analyticsdisabled true`
    
       which will prevent analytics from ever being sent when either
       of them have been set. "
    
    --------------------------------------------------------------------------------
    
    Ref: https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out

--- os_x/installs/install_homebrew.sh
+++ os_x/installs/install_homebrew.sh
@@ -14,6 +14,26 @@ main() {
 
     print_result $? 'Homebrew'
 
+    # --------------------------------------------------------------------------
+
+    # If something when wrong so far, don't continue any further
+
+    [ $? -ne 0 ] \
+        && exit 0
+
+    # --------------------------------------------------------------------------
+
+    # Opt-out of Homebrew's analytics
+    # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
+
+    HOMEBREW_GIT_CONFIG_FILE="$(brew --repository)/.git/config"
+
+    if [ "$(git config --file="$HOMEBREW_GIT_CONFIG_FILE" --get homebrew.analyticsdisabled)" != "true" ]; then
+        git config --file="$HOMEBREW_GIT_CONFIG_FILE" --replace-all homebrew.analyticsdisabled true &> /dev/null
+    fi
+
+    print_result $? "Homebrew (opt-out of analytics)"
+
 }
 
 main
