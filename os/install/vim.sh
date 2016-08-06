5aeef943de0a0c765914b8c76c8bd2489190ad62
Adam Richardson <adam.rickardson@gmail.com>
2016-08-06 20:09:08 +0100

    [vim] Add tern plugin

--- os/install/vim.sh
+++ os/install/vim.sh
@@ -18,7 +18,26 @@ install_plugins() {
         "rm -rf '$VUNDLE_DIR' \
             && git clone --quiet '$VUNDLE_GIT_REPO_URL' '$VUNDLE_DIR' \
             && printf '\n' | vim +PluginInstall +qall" \
-        "Install Vim plugins"
+        "Install plugins" \
+        || return 1
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Install additional things required by some plugins.
+
+    execute \
+        ". $HOME/.bash.local \
+            && cd $HOME/.vim/plugins/tern_for_vim \
+            && npm install" \
+        "Install plugins (extra installs for 'tern_for_vim')"
+
+}
+
+update_plugins() {
+
+    execute \
+        "vim +PluginUpdate +qall" \
+        "Update plugins"
 
 }
 
@@ -29,6 +48,7 @@ main() {
     print_in_purple "\n  Vim\n\n"
 
     install_plugins
+    update_plugins
 
 }
 
