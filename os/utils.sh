db0e371c870b033ec1a81b7543e18a8d7747d465
Adam Richardson <adam@zenelements.com>
2016-10-05 11:06:54 +0100

    [os] Fix `is_supported_version` from `utils.sh`

--- os/utils.sh
+++ os/utils.sh
@@ -181,6 +181,8 @@ is_supported_version() {
 
         if (( 10#${v1[i]} < 10#${v2[i]} )); then
             return 1
+        elif (( 10#${v1[i]} > 10#${v2[i]} )); then
+ +          return 0
         fi
 
     done
