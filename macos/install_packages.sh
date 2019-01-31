cf3170b1ea1f9d0f1278d7a0cad649708ec3ffd6
Adam Richardson <adam.rickardson@gmail.com>
2019-01-31 09:37:45 +0000

    Add `Quick Look` plugins to the install list

--- macos/install_packages.sh
+++ macos/install_packages.sh
@@ -16,7 +16,7 @@ if answer_is_yes; then
 	./install_homebrew.sh
 	./install_bash.sh
 
-	print_in_purple "\n  Brew packages \n\n"
+	print_in_purple "\n  Brew Packages \n\n"
 
 	brew_install "Ack!" "ack"
 	brew_install "FFmpeg" "ffmpeg"
@@ -26,11 +26,16 @@ if answer_is_yes; then
 	brew_install "Pinentry" "pinentry-mac"
 	brew_install "Zopfli" "zopfli"
 
-	print_in_purple "\n  Brewcask packages \n\n"
+	print_in_purple "\n  Brew Cask Packages \n\n"
 
 	brew_install "Docker" "docker" "caskroom/cask" "cask"
 	brew_install "Hyper" "hyper" "caskroom/cask" "cask"
 	brew_install "Visual Studio Code" "visual-studio-code" "caskroom/cask" "cask"
+
+	print_in_purple "\n  Brew Cask Quick Look Packages \n\n"
+
+	brew_install "JSON" "quicklook-json" "caskroom/cask" "cask"
+	brew_install "Markdown" "qlmarkdown" "caskroom/cask" "cask"
 
 	printf '\n'
 
